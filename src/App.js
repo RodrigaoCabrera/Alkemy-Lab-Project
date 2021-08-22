6a863a6efe2641a9bb44c10354f77f27c1a3492a
Angel Daniel Mejia <angeldmb94@gmail.com>
2021-08-22 12:55:51 +0000

    Members and Projects Forms added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import TestimonialForm from './Components/Testimonials/TestimonialsForm';
 import UserForm from './Components/Users/UsersForm';
 import SchoolCampaign from './Campaigns/School/SchoolCampaign';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
+import MembersForm from './Components/Members/MembersForm';
+import ProjectsForm from './Components/Projects/ProjectsForm';
 
 function App() {
   return (
@@ -24,6 +26,8 @@ function App() {
           <Route path="/backoffice/create-slide" component={SlidesForm} />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
+          <Route path="/create-member" component={MembersForm} />
+          <Route path="/create-project" component={ProjectsForm} />
           <Route path="/school-campaign" component={SchoolCampaign} />
           <Route path="/toys-campaign" component={ToysCampaign} />
         </Switch>
