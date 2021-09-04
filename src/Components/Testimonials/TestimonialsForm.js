d1b8d3bc19ab4af5fb371fe94d9c6b7b53410986
Joel Alfaro <alfarojoelf@gmail.com>
2021-09-04 14:30:14 +0000

    Merged in feature/OT65-31-formulario-edición---creación-de (pull request #7)
    Feature/OT65-31 formulario edición   creación de
    
    * add style, colors and title
    
    * code refactor
    
    * add submit data, post and put
    
    * add chakra-ui and errorHandlers
    
    * add formik and validation
    
    * add CKEditor library
    
    * add input image and convert to base64
    
    
    Approved-by: Pablo Gotelli

--- src/Components/Testimonials/TestimonialsForm.js
+++ src/Components/Testimonials/TestimonialsForm.js
@@ -1,32 +1,140 @@
 import React, { useState } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { Formik, Form, Field } from 'formik';
+import { Button, Box, Input, FormControl, FormLabel, FormErrorMessage, Text, Heading  } from '@chakra-ui/react';
+import axios from 'axios';
 import '../FormStyles.css';
 
-const TestimonialForm = () => {
-    const [initialValues, setInitialValues] = useState({
-       name: '',
-       description: '' 
-    });
+const TestimonialForm = ({ values }) => {
+  const [message, setMessage] = useState('');
+  const [error, seterror] = useState(false);
 
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'description'){
-            setInitialValues({...initialValues, description: e.target.value})
-        }
+  const [initialValues, setInitialValues] = useState({
+    name: values ? values.name : '',
+    description: values ? values.description : '',
+    image: ''
+  });
+
+  const nameHandler = (e) => {
+    setInitialValues({ ...initialValues, name: e.target.value});
+  };
+  const imageHandler = (e) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onload = function () {
+      setInitialValues({ ...initialValues, image: reader.result });
+    };
+  };
+
+  const validateName = () => {
+    if (initialValues.name.length >= 4) {
+      return;
+    } else if (initialValues.name.length == 0) {
+      return 'Este campo es obligatorio';
+    } else {
+      return 'Debe contener almenos 4 caracteres';
     }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
+  };
+  const validateDescription = () => {
+    if (initialValues.description.length > 0) {
+      return;
+    } else {
+      return 'Este campo es obligatorio';
     }
+  };
 
-    return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Testimonial Title"></input>
-            <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Testimonial description"></input>
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
-    );
-}
- 
+  const validateImage = () => {
+    if(initialValues.image.length > 0){
+      return;
+    }else{
+      return 'Debe subir una imagen';
+    }
+  };
+
+  const handleSubmit = async () => {
+    try {
+      let response;
+      let description = initialValues.description;
+      if(initialValues.description[0] === '<'){
+        description = description.slice(3, -4); //CKEditor return data whit <p></p>
+      }
+      let objectSend = {
+        name: initialValues.name,
+        description,
+        image: initialValues.image
+      };
+      console.log(objectSend);
+      if(values){
+        response = await axios.put('http://ongapi.alkemy.org/api/testimonials/'+ values.id, objectSend);
+      }else{
+        response = await axios.post('http://ongapi.alkemy.org/api/testimonials', objectSend);
+      }
+      if(response.data.success){
+        seterror(false);
+        setMessage(response.data.message);
+      }else{
+        seterror(true);
+        setMessage(response.data.message);
+      }
+    } catch (err) {
+      seterror(true);
+      setMessage('Error al enviar el testimonio');
+    }
+  };
+
+  return (
+    <Box maxW='sm' minH='65vh' borderWidth="1px" borderRadius="lg" p='3' boxShadow='5px 5px 15px rgba(0,0,0,0.3)'>
+      <Heading align='center' color='#6BBCFA' as="h1" size="lg" mb='3'>Testimonio</Heading>
+      <Formik className="form-container"
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+      >
+        {(props) => (
+          <Box as={Form} d='flex' minH='65vh' flexDirection='column' justifyContent='space-between'>
+            <Field name="name" validate={validateName}>
+              {({ field, form }) => (
+                <FormControl isInvalid={form.errors.name && form.touched.name} isRequired>
+                  <FormLabel htmlFor="name">Titulo</FormLabel>
+                  <Input {...field} value={initialValues.name} onChange={nameHandler} id="name" />
+                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+            <Field name='description' validate={validateDescription}>
+              {({ form }) => (
+                <FormControl isInvalid={form.errors.description && form.touched.description} isRequired>
+                  <FormLabel htmlFor="description" >Descripcion</FormLabel>
+                  <Field as={CKEditor} editor={ClassicEditor}
+                    data={initialValues.description}
+                    onChange={(event, editor) => {
+                      const data = editor.getData();
+                      setInitialValues({ ...initialValues, description: data });
+                    }}
+                    onBlur={validateDescription}
+                  />
+                  <FormErrorMessage>{form.errors.description}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+            <Field name="image" validate={validateImage}>
+              {({ form }) => (
+                <FormControl isInvalid={form.errors.image && form.touched.image} isRequired>
+                  <FormLabel htmlFor="image">Imagen</FormLabel>
+                  <Input pt='1' type="file" name='image' onChange={imageHandler} accept="image/jpeg , image/png" />
+                  <FormErrorMessage>{form.errors.image}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+            <Button mt={4} bg='#319795' isLoading={props.isSubmitting} type="submit" color='#fff' _hover={{bg: '#143E3D'}}>
+              Enviar
+            </Button>
+            {message.length > 0 && <Text align='center' color={error ? 'tomato' : '#4bc94f'}>{message}</Text>}
+          </Box>
+        )}
+      </Formik>
+    </Box>
+  );
+};
+
 export default TestimonialForm;
