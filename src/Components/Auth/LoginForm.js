f23d28b02df9fc87fa8a90fd04e702929da936c0
Matias Walter <matiwalter97@hotmail.com>
2021-09-06 05:38:52 +0000

    Merged in feature/OT65-26-formulario-login (pull request #6)
    Feature/OT65-26 formulario login
    
    * replace Chakra form components with Formik components
    
    * Merge branch 'master' into feature/OT65-26-formulario-login
    
    * add validation on blur
    
    * add Yup validation schema
    
    * add formik and chakra form components
    
    * add base login form
    
    
    Approved-by: Pablo Gotelli

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -1,33 +1,122 @@
 import React, { useState } from 'react';
-import '../FormStyles.css';
+import {
+  Box,
+  Flex,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  Button,
+  Heading,
+  Link,
+  Image,
+  Divider
+} from '@chakra-ui/react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
-const LoginForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        email: '',
-        password: ''
-    });
+export const LoginForm = () => {
 
-    const handleChange = (e) => {
-        if(e.target.name === 'email'){
-            setInitialValues({...initialValues, email: e.target.value})
-        } if(e.target.name === 'password'){
-            setInitialValues({...initialValues, password: e.target.value})
-        }
-    }
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-        localStorage.setItem('token', 'tokenValueExample')
-    }
+  const [formValues, setFormValues] = useState({
+    email: '',
+    password: ''
+  });
 
-    return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="email" value={initialValues.name} onChange={handleChange} placeholder="Enter email"></input>
-            <input className="input-field" type="text" name="password" value={initialValues.password} onChange={handleChange} placeholder="Enter password"></input>
-            <button className="submit-btn" type="submit">Log In</button>
-        </form>
-    );
-}
- 
-export default LoginForm;
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .email('Ingrese un email válido')
+      .required('Requerido'),
+    password: Yup.string()
+      .min(6, 'Debe tener un mínimo de 6 caracteres')
+      .max(16, 'Tu contraseña debe de tener entre 6 y 16 caracteres')
+      .required('Requerido')
+      .matches(
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/
+        ,'Debe tener al menos una letra, un número y un caracter especial (!@#$%)')
+  });
+
+  return (
+    <Flex 
+      height='100%'
+      direction='column'
+      alignItems='center'
+    >
+      <Box>
+        <Image 
+          src='/images/logo-ong.png'
+          alt='Logo Somos Más'
+          boxSize='200px'
+        />
+      </Box>
+      <Box 
+        padding={8}
+        minWidth={'sm' | 'md'}
+        borderWidth={1}
+        borderRadius={8}
+        boxShadow='lg'
+      >
+        <Box textAlign='center'>
+          <Heading as='h1'>Iniciar sesión</Heading>
+        </Box>
+        <Divider marginY={6} />
+        <Box marginY={4} textAlign='left'>
+          <Formik
+            initialValues={{
+              email: '',
+              password: ''
+            }}
+            validationSchema={validationSchema}
+            onSubmit={(values) => {
+              setFormValues(values);
+            }}
+          >
+            {({errors, touched}) => (
+              <Form>
+                <FormControl marginTop={2} isRequired isInvalid={touched.email && errors.email}>
+                  <FormLabel htmlFor='email'>Email</FormLabel>
+                  <Field
+                    as={Input}
+                    id='email'
+                    name='email'
+                    type='email'
+                    autoComplete='off'
+                  />
+                  <ErrorMessage component={FormErrorMessage} name='email' />
+                </FormControl>
+
+                <FormControl marginTop={6} isRequired isInvalid={touched.password && errors.password}>
+                  <FormLabel htmlFor='password'>Contraseña</FormLabel>
+                  <Field
+                    as={Input}
+                    id='password'
+                    name='password'
+                    type='password'
+                    autoComplete='off'
+                  />
+                  <ErrorMessage component={FormErrorMessage} name='password' />
+                </FormControl>
+
+                <Button
+                  width='full'
+                  marginTop={4}
+                  type='submit'
+                  backgroundColor='#9AC9FB'
+                  color='#000'
+                  _hover={{
+                    bg: '#5FA5ED'
+                  }}
+                  _active={{
+                    bg: '#5FA5ED'
+                  }}
+                >
+                  Iniciar sesión
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+        <Link href='/register'>Registrarse</Link>
+      </Box>
+    </Flex>
+  );
+};
