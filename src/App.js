20bc622f94431bae55e3073067b46dc7bc3d12a3
Alejandro Jesus Mederico González <mederico.alejandro1988@gmail.com>
2021-09-07 14:16:14 +0000

    Merged in feature/OT65-27-formulario-edición---creación-de (pull request #11)
    Feature/OT65-27 formulario edición   creación de
    
    * fixed data axios finish
    
    * fixed data axios
    
    * fix app.js and NewsForms
    
    * Merge branch 'master' into feature/OT65-27-formulario-edición---creación-de
    
    * fix final
    
    * fix console.log
    
    * Merge branch 'master' into feature/OT65-27-formulario-edición---creación-de
    
    * add chakra and CKedeitor
    
    * axios Post and Get and formik Yup
    
    
    Approved-by: Pablo Gotelli

--- src/App.js
+++ src/App.js
@@ -26,8 +26,8 @@ function App() {
           <Route path="/create-news" component={NewsForm} />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
           <Route path="/create-testimonials" component={TestimonialForm} />
-          <Route path="/create-user" component={UserForm} />
-          <Route path="/create-member" component={MembersForm} />
+          <Route path="/create-user" component={UserForm}/>
+          <Route path="/create-member" component={MembersForm}/>
           <Route path="/create-project" component={ProjectsForm} />
           <Route path="/school-campaign" component={SchoolCampaign} />
           <Route path="/toys-campaign" component={ToysCampaign} />
--- src/Components/News/NewsForm.js
+++ src/Components/News/NewsForm.js
@@ -1,41 +1,130 @@
-import React, { useState } from 'react';
+import {React,useEffect,useState} from 'react';
 import '../../Components/FormStyles.css';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+import {FormLabel,Button,Container,Alert,AlertIcon,Text} from '@chakra-ui/react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-const NewsForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        title: '',
-        content: '',
-        category: ''
-    });
-
-    const handleChange = (e) => {
-        if(e.target.name === 'title'){
-            setInitialValues({...initialValues, title: e.target.value})
-        } if(e.target.name === 'content'){
-            setInitialValues({...initialValues, content: e.target.value})
-        } if(e.target.name === 'category') {
-            setInitialValues({...initialValues, category: e.target.value})
-        }
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-    }
-
-    return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="title" value={initialValues.title || ''} onChange={handleChange}></input>
-            <input className="input-field" type="text" name="content" value={initialValues.content || ''} onChange={handleChange}></input>
-            <select className="select-field" name="category" value={initialValues.category || ''} onChange={handleChange}>
-                <option value="" disabled>Select category</option>
-                <option value="1">Demo option 1</option>
-                <option value="2">Demo option 2</option>
-                <option value="3">Demo option 3</option>
+const NewsForm = ({novedades}) => {
+  const [Categoria, setCategoria] = useState([]);
+  const [EnvioExitoso, setEnvioExitoso] = useState(false);
+  const [EnvioError, setEnvioError] = useState(false);
+  useEffect(() => {
+    const obtenerDatos = async() =>{
+      try {const  {data}  = await axios.get('http://ongapi.alkemy.org/api/categories');
+        setCategoria(data.data);
+      } catch (error) {console.log(error);}
+    };
+    obtenerDatos();
+  }, []);
+  return ( 
+    <Container mt={3}>
+      <Formik
+        initialValues={{
+          name: novedades ? novedades.name : '',
+          content: novedades ? novedades.content : '',
+          slug: '',
+          image: '',
+          id: novedades ? novedades.id : 0}}
+        onSubmit ={(formData,{resetForm})=>{ 
+          console.log(novedades === undefined);
+          if (novedades === undefined) {
+            console.log('hola sin props');
+            axios.post('http://ongapi.alkemy.org/api/news', {
+              name: formData.name,
+              content: formData.content,
+              slug: formData.slug,
+              image: formData.image
+            }).then(function (response) {
+              console.log(response);
+              setEnvioExitoso(true);
+              setTimeout(()=>{
+                setEnvioExitoso(false);
+                resetForm();
+              },3000);
+              resetForm();
+            }).catch(function (error) {
+              console.log('No se pudo ingresar por error :',error);
+              setEnvioError(true);
+              setTimeout(()=>{
+                setEnvioError(false);
+              },3000);
+            });} else {
+            axios.put(`http://ongapi.alkemy.org/api/news/${novedades.id}`, {
+              name: formData.name,
+              content: formData.content,
+              image: formData.image,
+              id: novedades.id,
+              slug: formData.slug,
+            }).then(function (response) {
+              console.log(response);
+              setEnvioExitoso(true);
+              setTimeout(()=>{
+                setEnvioExitoso(false);
+                resetForm();
+              },3000);
+              resetForm();
+            }).catch(function (error) {
+              console.log('No se pudo ingresar por error :',error);
+              setEnvioError(true);
+              setTimeout(()=>{
+                setEnvioError(false);
+              },3000);
+            });}
+        }}
+        validationSchema ={Yup.object({
+          name: Yup.string().required('Requiere title').min(4,'Minimo 4 caracteres'),
+          content: Yup.string().required('Requiere la content'),
+          slug: Yup.string().required('Requiere la category'),
+          image: Yup.string().required('Requiere la category'),
+        })}
+      >
+        {({handleSubmit,values,handleChange,handleBlur,errors,touched,initialValues})=>(
+        
+          <form  className="form-container" onSubmit={handleSubmit}>
+            <Text as="samp" fontSize="2xl" color="teal.500" marginTop={7} textAlign="center" fontWeight="bold">Formulario novedades</Text>
+            <FormLabel htmlFor="name">First name</FormLabel>
+            <input className="input-field" id="name"type=" text" name="name" value={values.name} onChange={handleChange} onBlur={handleBlur}></input>
+            {touched.name && errors.name && <Alert status="error"><AlertIcon />{errors.name}</Alert>}
+            <FormLabel htmlFor="content">Content</FormLabel>
+            <CKEditor
+              editor={ ClassicEditor }
+              data={initialValues.content}
+              onChange={ ( event, editor ) => values.content=editor.getData().slice(3,-4)}
+            // onBlur={ ( event, editor ) => values.content.length === 0 ? ErrorConst= true : ErrorConst=false}
+            />
+            {errors.content && <Alert status="error"><AlertIcon />{errors.content}</Alert>}
+            <FormLabel htmlFor="slug">Categoria</FormLabel>
+            <select className="select-field" id="slug" name="slug" onChange={handleChange} onBlur={handleBlur}>
+              <option value="" disabled >Select category</option>
+              {Categoria === []
+                ?<p>Falta Categoria</p>
+                :Categoria.map((e,index) =>
+                  <option key={index} value={e.name}>{e.name}</option>)}
             </select>
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
-    );
-}
- 
+            {touched.slug && errors.slug && <Alert status="error"><AlertIcon />{errors.slug}</Alert>}
+            <FormLabel htmlFor="image">Imagen</FormLabel>
+            <input className="input-field" 
+              id="image" 
+              type="file" 
+              name="image" 
+              onBlur={handleBlur} 
+              onChange={(e)=>{
+                const reader = new FileReader();
+                reader.readAsDataURL(e.target.files[0]);
+                reader.onload = function () {values.image=reader.result;};
+              }} 
+              accept="image/gif ,image/jpeg, image/png"></input>
+            {touched.image && errors.image &&<Alert status="error"><AlertIcon />{errors.image}</Alert>}
+            <Button  mt={4} colorScheme="teal" type="submit">Send</Button>
+            {EnvioExitoso && <Alert status="success"><AlertIcon />Novedad Enviada</Alert>}
+            {EnvioError && <Alert status="error"><AlertIcon />Novedad No enviado</Alert>}
+          </form >
+        )}
+      </Formik>
+    </Container>
+  );
+};
 export default NewsForm;
