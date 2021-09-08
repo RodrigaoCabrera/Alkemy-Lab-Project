6204cc4791bb8be588d17fbdca1eecfe69613da0
Joel Alfaro <alfarojoelf@gmail.com>
2021-09-08 13:42:30 +0000

    Merged in feature/OT65-51-componente-screen-dashboard-back (pull request #18)
    Render text welcome
    
    * Render text welcome
    
    
    Approved-by: Pablo Gotelli

---
+++ src/Components/Backoffice/DashboardHome.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Box, Text } from '@chakra-ui/react';
+
+const DashboardHome = () => {
+  return (
+    <Box w='100%'>
+      <Text as='h1' textAlign='center' 
+        fontSize={{base: '25px', md: '40px'}}
+        color='#398be1' fontWeight='700'
+      >
+       Bienvenido al Backoffice
+      </Text>
+    </Box>
+  );
+};
+
+export default DashboardHome;
