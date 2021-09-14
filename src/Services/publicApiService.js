353cfe3d4dd561dddd4f615e8bfeef400124f738
Cabrera Rodrigo <rodrigod33d@gmail.com>
2021-09-14 03:33:51 +0000

    Merged in feacture/OT65-74-desarrollar-servicio-metodos-reutilizables-para-petición-get (pull request #33)
    Desarrollar servicio y metodos reutilizables para realizar una petición GET a las rutas públicas de la API
    
    * Get method that receives URL and id and calls the API
    
    * adding the header to the get method
    
    * PR fixes: merge to solve problem
    
    * Pr fixes: merge to solve problem
    
    
    Approved-by: Pablo Gotelli

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -1,10 +1,23 @@
 import axios from 'axios';
 const config = {
-  headers: {
-    Group: 65                //Aqui va el ID del equipo!!
-  }
+   headers: {
+        Group: 65                //Aqui va el ID del equipo!!
+    }
+}
+
+const Get = (URL, id, config) => {
+    const enlace = `${URL}/${id}`;
+
+    if (id === undefined) {
+        axios.get(URL, { header: { config } })
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    } else {
+        axios.get(enlace, { header: { config } })
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }
 };
-  
 
 export const Post =   async(link,data) => {
   axios.post(`${link}`, data,config)
@@ -15,3 +28,5 @@ export const Post =   async(link,data) => {
       console.log(error);
     });
 };
+
+
