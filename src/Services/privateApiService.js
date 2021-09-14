7f8a94f12c7831b7be32161b71c2f9828ecbf97d
Pedro Copes <petercopes.96@gmail.com>
2021-09-14 02:21:26 +0000

    Merged in feature/OT65-71-crear-método-reutilizable-que-re (pull request #26)
    Feature/OT65-71 crear método reutilizable que re
    
    * put request method created.
    
    * fixed minor issues
    
    * Merge branch 'master' into feature/OT65-71-crear-método-reutilizable-que-re
    
    * Merge branch 'master' into feature/OT65-71-crear-método-reutilizable-que-re
    
    * updated putRequest
    
    * added return statement for better error management
    
    * Merge branch 'master' into feature/OT65-71-crear-método-reutilizable-que-re
    
    * Merge branch 'master' into feature/OT65-71-crear-método-reutilizable-que-re
    
    
    Approved-by: Pablo Gotelli

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -11,6 +11,21 @@ const Get = () => {
     .then(res => console.log(res))
     .catch(err => console.log(err));
 };
+export const PutRequest = async (url,id,data) =>{
+  const requestURL = `${url}/${id}`;
+  const header = verifyTokenAuthorization();
+  try {
+    const res = await axios.put(requestURL, data,{headers: header});
+    if(!(res.status===200||res.status===204)){
+      throw new Error(res.status);
+    }else{
+      return res;
+    }
+  } catch (error) {
+    return error;
+  }
+};
+
 
 export const PostRequest = async (url, data) => {
   const header = verifyTokenAuthorization();
