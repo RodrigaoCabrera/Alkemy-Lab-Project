bca41aa73615092a7cefa9ae6591bda8ca36aadd
Joel Alfaro <alfarojoelf@gmail.com>
2021-09-25 14:45:05 +0000

    Merged in feature/OT65-115-integrar-leaflet-maps-en-screen (pull request #71)
    Feature/OT65-115 integrar leaflet maps en screen
    
    * install leafletmaps library
    
    * integration leaflet maps
    
    * add get position and show errors
    
    * pull master
    
    
    Approved-by: Pablo Gotelli

--- src/Components/Contact/ContactPage.jsx
+++ src/Components/Contact/ContactPage.jsx
@@ -1,16 +1,137 @@
-import React, { useState, useEffect } from 'react';
-import { Flex, Spacer, Heading, List,Button, VStack } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { Flex, Spacer, Heading, List,Button, VStack, Text} from '@chakra-ui/react';
 import TitlePages from '../UI/TitlePages';
 import ContactForm from './ContactForm';
+import 'leaflet/dist/leaflet.css';
 import { GetContact } from '../../Services/ContactService';
+import L from 'leaflet';
+import axios from 'axios';
 import { AiOutlineArrowLeft} from 'react-icons/ai';
 import ContactInfoItem from './ContactInfoItem';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+const mockData = [
+  {
+    'id': 2,
+    'name': 'asdqwe',
+    'email': 'andreagallo264@gmail.com',
+    'phone': '[object Undefined]',
+    'message': 'mensaje de prueba',
+    'deleted_at': null,
+    'created_at': '2021-04-27T15:57:11.000000Z',
+    'updated_at': '2021-04-27T15:57:11.000000Z',
+    'group_id': null
+  },
+  {
+    'id': 3,
+    'name': 'asdqwe',
+    'email': 'asd@asd.com',
+    'phone': '[object Undefined]',
+    'message': 'qqqqqqqqqqqqqqqqqqqqqqq',
+    'deleted_at': null,
+    'created_at': '2021-04-27T16:00:27.000000Z',
+    'updated_at': '2021-04-27T16:00:27.000000Z',
+    'group_id': null
+  },
+  {
+    'id': 4,
+    'name': 'asdqwe',
+    'email': 'asd@asd.com',
+    'phone': '[object Undefined]',
+    'message': 'qqqqqqqqqqqqqqqqqqqqqqq',
+    'deleted_at': null,
+    'created_at': '2021-04-27T16:00:48.000000Z',
+    'updated_at': '2021-04-27T16:00:48.000000Z',
+    'group_id': null
+  },
+  {
+    'id': 5,
+    'name': 'asdqwe',
+    'email': 'asd@asd.com',
+    'phone': '2133212321',
+    'message': 'qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq',
+    'deleted_at': null,
+    'created_at': '2021-04-27T16:02:12.000000Z',
+    'updated_at': '2021-04-27T16:02:12.000000Z',
+    'group_id': null
+  },
+  {
+    'id': 6,
+    'name': 'asdqwe',
+    'email': 'asd@asd.com',
+    'phone': '13223132',
+    'message': 'ddddddddddddddddddddddddddddddddd',
+    'deleted_at': null,
+    'created_at': '2021-04-27T16:03:19.000000Z',
+    'updated_at': '2021-04-27T16:03:19.000000Z',
+    'group_id': null
+  },
+  {
+    'id': 7,
+    'name': 'asdqwe',
+    'email': 'andreagallo264@gmail.com',
+    'phone': '6545654556',
+    'message': 'eeeeeeeeeeeeeeeeeeeeeeee',
+    'deleted_at': null,
+    'created_at': '2021-04-27T16:04:14.000000Z',
+    'updated_at': '2021-04-27T16:04:14.000000Z',
+    'group_id': null
+  },
+  {
+    'id': 8,
+    'name': 'asdqwe',
+    'email': 'andreagallo264@gmail.com',
+    'phone': '45645656',
+    'message': 'qqqqqqqqqqqqqqqqqqqqqq',
+    'deleted_at': null,
+    'created_at': '2021-04-27T16:05:05.000000Z',
+    'updated_at': '2021-04-27T16:05:05.000000Z',
+    'group_id': null
+  },
+  {
+    'id': 9,
+    'name': 'asdqwe',
+    'email': 'asd@asd.com',
+    'phone': '456456456',
+    'message': 'este es un mensaje de prueba',
+    'deleted_at': null,
+    'created_at': '2021-04-27T16:16:42.000000Z',
+    'updated_at': '2021-04-27T16:16:42.000000Z',
+    'group_id': null
+  },
+  {
+    'id': 10,
+    'name': 'Juan',
+    'email': 'gabriel.rios@intive.com',
+    'phone': '111111111',
+    'message': 'xdasdsadsada',
+    'deleted_at': null,
+    'created_at': '2021-05-05T18:10:47.000000Z',
+    'updated_at': '2021-05-05T18:10:47.000000Z',
+    'group_id': null
+  },
+  {
+    'id': 11,
+    'name': 'asdqwe',
+    'email': 'asd@asd.com',
+    'phone': '132213213',
+    'message': 'aaaaaaaaaaaaaaaaaaaaaa',
+    'deleted_at': null,
+    'created_at': '2021-05-05T18:21:27.000000Z',
+    'updated_at': '2021-05-05T18:21:27.000000Z',
+    'group_id': null
+  }];
+
 const ContactPage = () =>{
 
   const [onForm, setOnForm] = useState(false);
+  const [position, setPosition] = useState([]);
+  const [error, setError] = useState(false);
   const [ContactData, setContactData] = useState([]);
 
-  useEffect(() => {
+  useEffect(async () => {
     GetContact()
       .then((response) => {
         setContactData(response.data);
@@ -18,7 +139,23 @@ const ContactPage = () =>{
       .catch((error) => {
         console.log(error);
       });
+    getPosition();
   }, [setContactData]);
+
+  const getPosition = async () => {
+    const response = await axios.get('http://ongapi.alkemy.org/api/organization');
+    if(response.data.success){
+      let addressString = response.data.data.address; //la direccion esta en un array de string
+      let posComa = addressString.indexOf(','); // calculo donde esta la coma
+      let lat = addressString.slice(1, posComa); // agarrro el primer elemento
+      let lon = addressString.slice(posComa + 2, -1); // agarro el segundo elemento
+      setPosition([parseFloat(lat, 10), parseFloat(lon, 10)]); // lo paso a float y lo agrego al array
+      setError(false);
+    }
+    else{
+      setError(true);
+    }
+  };
 
   const contactInfo = 
     (
@@ -66,12 +203,33 @@ const ContactPage = () =>{
     <ContactForm/>
   </>
   );
+
+  let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+  });
+
+  L.Marker.prototype.options.icon = DefaultIcon;
+
+  
   return(
     <Flex direction='column'>
       <TitlePages text={'Contacto'} />
       <Spacer/>
-      <Flex m='5px' align='center' p='20px' justify='center'>
+      <Flex m='5px' align='center' p='20px' justify='center' direction={{ base: 'column', md: 'row' }}>
         {onForm ? formInfo : contactInfo }
+        {position.length > 0 && !error ? 
+          (
+            <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+              <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={position}/>
+            </MapContainer>
+          ):
+          (<Text fontSize="md" color='red' ml={{base: '0', md:'120px' }}>Error al cargar la direccion de la ONG</Text>) 
+        }
       </Flex>
     </Flex>
   );
