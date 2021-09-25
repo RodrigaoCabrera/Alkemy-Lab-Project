831e60ca02df033bcb8045eca0d77ec7845cbcdd
Matias Walter <matiwalter97@hotmail.com>
2021-09-25 14:38:21 +0000

    Merged in feature/OT65-86-crear-variables-de-entorno (pull request #66)
    Feature/OT65-86 crear variables de entorno
    
    * Merge branch 'master' into feature/OT65-86-crear-variables-de-entorno
    
    * add variable to env.example
    
    * add environment variable
    
    
    Approved-by: Pablo Gotelli

--- src/Services/categoryService.js
+++ src/Services/categoryService.js
@@ -1,22 +1,25 @@
 import { DeleteRequest, GetRequest, PostRequest, PutRequest } from './privateApiService';
-const CATEGORIES_URL = 'http://ongapi.alkemy.org/api/categories';
+
+// eslint-disable-next-line no-undef
+const url = process.env.REACT_APP_CATEGORIES;
+
 export const getCategories = async ()=>{
-  const response = await GetRequest(CATEGORIES_URL,undefined);
+  const response = await GetRequest(url);
   return response;
 };
 export const getCategory = async (id)=>{
-  const response = await GetRequest(CATEGORIES_URL,id);
+  const response = await GetRequest(url, id);
   return response;
 };
 export const postCategory = async (data)=>{
-  const response = await PostRequest(CATEGORIES_URL,data);
+  const response = await PostRequest(url, data);
   return response;
 };
 export const putCategory =  async (id,data)=>{
-  const response = await PutRequest(CATEGORIES_URL,id,data);
+  const response = await PutRequest(url, id, data);
   return response;
 };
 export const deleteCategory = async (id)=>{
-  const response = await DeleteRequest(CATEGORIES_URL,id);
+  const response = await DeleteRequest(url, id);
   return response;
 };
