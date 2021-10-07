c0df8b81f5837e32ca26bb43c130740a69921e95
Maria Laura Navoni <lauranavonigomez@gmail.com>
2021-10-07 05:19:18 +0000

    Merged in feature/OT65-136-estructurar-componente-footer (pull request #104)
    estructurar componente footer
    
    * add fixes in app
    
    * add changes app
    
    * mobile first
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t65-project into feature/OT65-136-estructurar-componente-footer
    
    * add responsive
    
    
    Approved-by: Pablo Gotelli

--- src/Campaigns/Toys/Footer.js
+++ src/Campaigns/Toys/Footer.js
@@ -1,11 +1,281 @@
 import React from 'react';
+import { Image } from '@chakra-ui/image';
+import { Box, Link, Text, Flex, Heading, } from '@chakra-ui/layout';
+import { SiFacebook, SiInstagram, SiLinkedin, SiTwitter } from 'react-icons/si';
+import ToyCampaignLogo from './ToyCampaignLogo.png';
+import { useMediaQuery } from '@chakra-ui/media-query';
 
-const Footer = () => {
+const Footer = () => 
+
+{
+  const [isTabletSize, isLaptopSize, isSmartTvSize] = useMediaQuery([
+    '(min-width: 768px)',
+    '(min-width: 1024px)',
+    '(min-width: 1920px)',
+  ]);
+  const onMobile = !(isTabletSize || isLaptopSize || isTabletSize || isSmartTvSize);
+  const onTablet = (isTabletSize && !(isLaptopSize || isSmartTvSize));
+  const onLaptop = (isLaptopSize && !(isSmartTvSize));
+  const onSmartTv = (isSmartTvSize);
+
+
+  const baseContent = (
+    <Flex w='100%' h='15vh' align='center' justify='center' p={3}>
+      <Image h='80%' src={ToyCampaignLogo} />
+      <Box display='flex'>
+        <SiInstagram color='white' fontSize='20px' />
+        <SiFacebook color='white' fontSize='20px' />
+        <SiLinkedin color='white' fontSize='20px' />
+        <SiTwitter color='white' fontSize='20px' />
+        <Heading><Text as='span' color='blue'>Visita Nuestra Web</Text></Heading>
+        <Image
+          src='/images/logo-ong.png'
+          alt='Logo Somos MAS'
+          boxSize='200px'
+        /> </Box>
+    </Flex>
+  );
+
+  const tabletContent = (
+    <Flex w='100%' h='15vh' align='center' justify='space-around' p={3}>
+      <Box display='flex'>
+        <SiInstagram color='white' fontSize='20px' />
+        <SiFacebook color='white' fontSize='20px' />
+        <SiLinkedin color='white' fontSize='20px' />
+        <SiTwitter color='white' fontSize='20px' />
+        <Heading><Text as='span' color='blue'>Visita Nuestra Web</Text></Heading>
+        <Image
+          src='/images/logo-ong.png'
+          alt='Logo Somos MAS'
+          boxSize='200px'
+        /> </Box>
+    </Flex>
+  );
+
+  const laptopContent = (
+    <Flex w='100%' h='15vh' align='center' justify='space-around' p={3}>
+      <Box display='flex'>
+        <SiInstagram color='white' fontSize='20px' />
+        <Link
+          isExternal
+          href='https://www.instagram.com/SomosMÃ¡s'
+          marginRight='15px'
+          display='flex'
+          flexDirection='row'
+          alignItems='center'
+        >
+
+          <Text
+            marginLeft='10px'
+            fontSize='20px'
+            display={{
+              base: 'none',
+              lg: 'inline'
+            }}
+          >
+              Instagram
+          </Text>
+        </Link>
+        <SiFacebook color='white' fontSize='20px' /><Link
+          isExternal
+          href='https://www.facebook.com/Somos_MÃ¡s'
+          marginRight='15px'
+          display='flex'
+          flexDirection='row'
+          alignItems='center'
+        >
+
+          <Text
+            marginLeft='10px'
+            fontSize='20px'
+            display={{
+              base: 'none',
+              lg: 'inline'
+            }}
+          >
+              Facebook
+          </Text>
+        </Link>
+        <SiLinkedin color='white' fontSize='20px' />
+        <Link
+          isExternal
+          href='https://www.linkedin.com/company/somosmas'
+          marginRight='15px'
+          display='flex'
+          flexDirection='row'
+          alignItems='center'
+        >
+
+          <Text
+            marginLeft='10px'
+            fontSize='20px'
+            display={{
+              base: 'none',
+              lg: 'inline'
+            }}
+          >
+              Linkedin
+          </Text>
+        </Link>
+        <SiTwitter color='white' fontSize='20px' />
+        <Link
+          isExternal
+          href='https://www.twitter.com/somosmas'
+          display='flex'
+          flexDirection='row'
+          alignItems='center'
+        >
+
+          <Text
+            marginLeft='10px'
+            fontSize='20px'
+            display={{
+              base: 'none',
+              lg: 'inline'
+            }}
+          >
+              Twitter
+          </Text>
+        </Link>
+        <Heading><Text as='span' color='blue'>Visita Nuestra Web</Text></Heading>
+        <Image
+          src='/images/logo-ong.png'
+          alt='Logo Somos MÃ¡s'
+          boxSize='200px'
+        /> </Box>
+    </Flex>
+  );
+  const smartTvContent = (
+    <Flex w='100%' h='15vh' align='center' justify='space-around' p={3}>
+      <Box display='flex'>
+        <SiInstagram color='white' fontSize='20px' />
+        <Link
+          isExternal
+          href='https://www.instagram.com/SomosMÃ¡s'
+          marginRight='15px'
+          display='flex'
+          flexDirection='row'
+          alignItems='center'
+        >
+
+          <Text
+            marginLeft='10px'
+            fontSize='20px'
+            display={{
+              base: 'none',
+              lg: 'inline'
+            }}
+          >
+              Instagram
+          </Text>
+        </Link>
+        <SiFacebook color='white' fontSize='20px' /><Link
+          isExternal
+          href='https://www.facebook.com/Somos_MÃ¡s'
+          marginRight='15px'
+          display='flex'
+          flexDirection='row'
+          alignItems='center'
+        >
+
+          <Text
+            marginLeft='10px'
+            fontSize='20px'
+            display={{
+              base: 'none',
+              lg: 'inline'
+            }}
+          >
+              Facebook
+          </Text>
+        </Link>
+        <SiLinkedin color='white' fontSize='20px' />
+        <Link
+          isExternal
+          href='https://www.linkedin.com/company/somosmas'
+          marginRight='15px'
+          display='flex'
+          flexDirection='row'
+          alignItems='center'
+        >
+
+          <Text
+            marginLeft='10px'
+            fontSize='20px'
+            display={{
+              base: 'none',
+              lg: 'inline'
+            }}
+          >
+              Linkedin
+          </Text>
+        </Link>
+        <SiTwitter color='white' fontSize='20px' />
+        <Link
+          isExternal
+          href='https://www.twitter.com/somosmas'
+          display='flex'
+          flexDirection='row'
+          alignItems='center'
+        >
+
+          <Text
+            marginLeft='10px'
+            fontSize='20px'
+            display={{
+              base: 'none',
+              lg: 'inline'
+            }}
+          >
+              Twitter
+          </Text>
+        </Link>
+        <Heading><Text as='span' color='blue'>Visita Nuestra Web</Text></Heading>
+        <Box
+          display={{
+            base: 'none',
+            '2xl': 'flex'
+          }}
+          flexDirection='column'
+          alignItems='center'
+        >
+          <Text fontSize='25px'>Otras campañas</Text>
+          <Link to=''>
+            <Text fontSize='20px'>Juguetes</Text>
+          </Link>
+          <Link to=''>
+            <Text fontSize='20px'>Escuela</Text>
+          </Link>
+        </Box>
+        <Image
+          src='/images/logo-ong.png'
+          alt='Logo Somos MÃ¡s'
+          boxSize='200px'
+        /> </Box>
+    </Flex>
+  );
+  let content;
+  if (onMobile) {
+    content = baseContent;
+  } else if (onTablet) {
+    content = tabletContent;
+  }
+  else if (onLaptop) {
+    content = laptopContent;
+  }
+  else if (onSmartTv) {
+    content = smartTvContent;
+  }
   return (
-    <div>
-      
-    </div>
+    <Flex w='100%' h='15vh'>
+      {content}
+    </Flex>
+
   );
 };
+<<<<<<< HEAD
+
+=======
  
+>>>>>>> 9f9f8bdd7006cef004bc1976690b62a0609f5e3a
 export default Footer;
